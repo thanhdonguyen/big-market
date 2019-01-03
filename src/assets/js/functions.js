eeb3f49571cc6bd139f0dc883c049e1efe6e8ea7
thanhdo <thanhdo@allgrow-labo.jp>
2019-01-03 17:14:23 +0700

    commit 2

---
+++ src/assets/js/functions.js
@@ -0,0 +1,29 @@
+$(function(){
+	$('.mini-cart').hover(function(){
+      	$(this).closest('.header-top__cart')
+      	.find('.cart-content')
+      	.css('visibility','visible')
+      	.show()
+      	.addClass('is_cartHover');
+    },function(){
+      	$(this).closest('.header-top__cart')
+      	.find('.cart-content')
+      	.css('visibility','visible')
+      	.hide()
+      	.removeClass('is_cartHover');
+    });
+
+    $('.cart-content').hover(function(){
+      	$(this).closest('.header-top__cart')
+      	.find('.cart-content')
+      	.css('visibility','visible')
+      	.show()
+      	.addClass('is_cartHover');
+    },function(){
+      	$(this).closest('.header-top__cart')
+      	.find('.cart-content')
+      	.css('visibility','visible')
+      	.hide()
+      	.removeClass('is_cartHover');
+    });
+})
