6aff7f1a14f0a990efa4d4c2a1b8acc51ac4b21f
thanhdo <thanhdo@allgrow-labo.jp>
2019-03-01 16:54:07 +0700

    commit 6

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -50,4 +50,27 @@ $(function(){
         nextArrow: '<span class="next-sale"><i class="fas fa-angle-right"></i></span>'
     });
     
+    var scrollTopLast = 0;
+    $(window).scroll(function () {
+        // var widthDevice = $(window).width();
+        if ($(window).scrollTop() > $('.header-top').height()) {
+            var scrollTopNow = $(this).scrollTop();
+            var $pageheader = $('.header-top');
+            // Reveal sticky nav on scroll-up
+            if(scrollTopNow > 100 && scrollTopNow > scrollTopLast) {
+                //Scroll down
+                $pageheader.removeClass('is_sticky');
+            } else if(scrollTopNow < scrollTopLast) {
+                //Scroll up
+                $pageheader.addClass('is_sticky');
+            }
+            scrollTopLast = scrollTopNow;
+            // $('.is_sticky').css('transform','translateY(0)')
+        }else{
+            $('.header-top').removeClass('is_sticky');
+        }
+
+       
+    });
+
 })
